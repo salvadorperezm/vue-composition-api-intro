49259e20b1fa03c85640900792aba164df98dfc7
salvadorperezm <salvadorperezm115@gmail.com>
2023-06-20 18:19:21 -0600

    Expense Tracker
    Initial styling added to match the page

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,70 @@
 <template>
-  <section>
-    <h2>Computed properties</h2>
-    <h3>{{ displayFullName }}</h3>
-    <div>
-      <input type="text" placeholder="First name" v-model.trim="firstName">
-      <input type="text" placeholder="Last name" v-model.trim="lastName">
-    </div>
+  <header class="header">
+    <h1 class="header__title">expense tracker</h1>
+  </header>
+  <section class="section">
+    <p class="section__text">Available funds: 200</p>
+    <p class="section__text--mb">Total expenses: 0</p>
+    <hr />
+    <p class="section__text--mt">Funds left: 100</p>
+  </section>
+  <section class="section">
+    <h2>Amount</h2>
+    <input type="number" placeholder="0" class="section__input">
+    <button class="section__button">Add Expense</button>
   </section>
 </template>
 
 <script setup>
-import { computed, ref, watch } from 'vue';
-
-
-const firstName = ref('');
-const lastName = ref('');
-
-const fullName = computed(() => {
-  return `${firstName.value} ${lastName.value}`
-})
-
-const displayFullName = computed(() => {
-  return !fullName.value.trim() ? "No name input yet" : fullName.value
-})
-
-watch(firstName, () => {
-  if (firstName.value.toLowerCase() === 'salvador') {
-    alert('You guest the name')
-  }
-})
 </script>
 
 <style scoped>
-.show {
-  display: block;
+.header {
+  background-color: #2c0966;
+  color: #ffffff;
+  padding: 32px;
+  text-align: center;
 }
 
-.hide {
-  display: none;
+.header__title {
+  font-size: 48px;
+  text-transform: capitalize;
+}
+
+.section {
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  padding: 16px;
+  margin: 40px auto;
+  max-width: 600px;
+}
+
+.section__button {
+  background-color: #2c0966;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+  display: block;
+  font-size: 16px;
+  margin-block-start: 10px;
+  padding: 8px 16px;
+}
+
+.section__input {
+  width: 100%;
+}
+
+.section__text,
+.section__text--mb,
+.section__text--mt {
+  font-size: 18px;
+}
+
+.section__text--mb {
+  margin-block-end: 10px;
+}
+
+.section__text--mt {
+  margin-block-start: 10px;
 }
 </style>
